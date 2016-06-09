de33785e36a70f6b5824103e0d8fe3f6e1f8edfb
Griffin Michl <griffinmichl@gmail.com>
2016-06-08 17:44:56 -0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 git checkout gh-pages
 
-git merge master
+git merge master --no-commit
+
+elm-make Main.elm --output=index.html
+
+git add .
+
+git commit -m "deploy"
 
 git push origin gh-pages
